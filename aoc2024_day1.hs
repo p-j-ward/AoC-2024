dc6c1b3e70f56c1ba232479c263b792cd765c6c7
p-j-ward <113056043+p-j-ward@users.noreply.github.com>
2024-12-08 16:44:37 +0000

    Slightly neatened
    Removed zipping then unzipping of sorted lists, now they are first stored unzipped, then zipped as needed to calculate elementwise diffs

--- aoc2024_day1.hs
+++ aoc2024_day1.hs
@@ -8,16 +8,15 @@ main = do
     inputString <- readFile "input_day1.txt"
     let inputPairs = map (map read . words) (lines inputString) :: [[Int]]
 
-    -- sort the two lists, store result as list of tuple sortedPairs
-    let sortedPairs = zip (sort (map head inputPairs)) (sort (map last inputPairs))
+    -- sort the two lists
+    let leftList = sort (map head inputPairs)
+    let rightList = sort (map last inputPairs)
 
     -- part1: we want the sum total of the (magnitude of the) elementwise differences
-    let diffs = map (\x -> (fst x) - (snd x)) sortedPairs
+    let diffs = map (\x -> (fst x) - (snd x)) (zip leftList rightList)
     let ans1 = sum (map abs diffs)
 
     -- part2: similarity score, the sum total of - each number in the left list, multiplied by the number of times it appears in the right list
-    let leftList = map fst sortedPairs
-    let rightList = map snd sortedPairs
     let similarity_score_for_element x = x * (length (filter (== x) rightList))
     let ans2 = sum (map similarity_score_for_element leftList)
 
