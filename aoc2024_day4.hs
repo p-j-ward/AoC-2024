3c081e8cd29b04347cbebc9bb9c3973971a0bbf8
p-j-ward <113056043+p-j-ward@users.noreply.github.com>
2024-12-09 19:53:37 +0000

    Added day 4 part 2 solution

--- aoc2024_day4.hs
+++ aoc2024_day4.hs
@@ -17,6 +17,20 @@ countInLeadingDiagonalBlock (a, b, c, d) = length (filter matchTest (zip4 (tail 
 countInTrailingDiagonalBlock :: ([Char], [Char], [Char], [Char]) -> Int
 countInTrailingDiagonalBlock (a, b, c, d) = length (filter matchTest (zip4 a (tail b) (tail (tail c)) (tail (tail (tail d)))))
 
+-- part 2
+-- we'll define our match test function to look at the X as follows:
+--   tl-tr
+--   --m--
+--   bl-br
+matchTestPart2 :: (Char, Char, Char, Char, Char) -> Bool
+matchTestPart2 (tl, tr, m, bl, br) = (m == 'A')                                                             -- for a match, middle must always be an A
+                                        && (((tl == 'M') && (br == 'S')) || ((tl == 'S') && (br == 'M')))   -- match on leading diagonal
+                                        && (((tr == 'M') && (bl == 'S')) || ((tr == 'S') && (bl == 'M')))   -- match on trailing diagonal
+
+-- we'll iterate with a 'window' in the same way we counted the diagonals in part 1
+countPart2MatchesInBlock :: ([Char], [Char], [Char]) -> Int
+countPart2MatchesInBlock (line1, line2, line3) = length (filter matchTestPart2 (zip5 line1 (tail (tail line1)) (tail line2) line3 (tail (tail line3))))
+
 
 main = do
     -- read and parse input into list
@@ -31,6 +45,14 @@ main = do
     let diagonalTestSlidingWindow = zip4 inputLines (tail inputLines) (tail (tail inputLines)) (tail  (tail (tail inputLines)))
     let leadingDiagonalMatchCount = sum (map countInLeadingDiagonalBlock diagonalTestSlidingWindow)
     let trailingDiagonalMatchCount = sum (map countInTrailingDiagonalBlock diagonalTestSlidingWindow)
-    
+
     let ans1 = horizontalMatchCount + verticalMatchCount + leadingDiagonalMatchCount + trailingDiagonalMatchCount
+
+    -- part 2
+    -- as per diagonals in part 1, now "window" is 3 lines at a time
+    let part2TestSlidingWindow = zip3 inputLines (tail inputLines) (tail (tail inputLines))
+    let ans2 = sum (map countPart2MatchesInBlock part2TestSlidingWindow)
+
+    
     print ans1  -- 2543
+    print ans2  -- 1930
