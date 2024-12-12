fd96a3fb3667da345d00da4b74d4ac23bbd84ea7
p-j-ward <113056043+p-j-ward@users.noreply.github.com>
2024-12-12 21:58:02 +0000

    Improved 1D zipper functions with Maybe for left and right, and added 2d functions

--- aoc2024_day6.hs
+++ aoc2024_day6.hs
@@ -1,18 +1,55 @@
 -- Advent of Code 2024, Day 6
 
--- list zipper functions
-zipperSelect :: ([a], a, [a]) -> a
+-- 1D list zipper functions
+type ZipperList a = ([a], a, [a])
+
+zipperSelect :: ZipperList a -> a
 zipperSelect (zl, v, zr) = v
 
-zipperSet :: ([a], a, [a]) -> a -> ([a], a, [a])
+zipperSet :: ZipperList a -> a -> ZipperList a
 zipperSet (zl, v, zr) newVal = (zl, newVal, zr)
 
-zipperLeft :: ([a], a, [a]) -> ([a], a, [a])
-zipperLeft (zl, v, zr) = (tail zl, head zl, v : zr)
+zipperLeft :: ZipperList a -> Maybe (ZipperList a)
+zipperLeft (zl, v, zr) = 
+    case zl of
+        [] -> Nothing -- left list is empty, can't go left any further
+        x:xs -> Just (xs, x, v:zr)
 
-zipperRight :: ([a], a, [a]) -> ([a], a, [a])
-zipperRight (zl, v, zr) = (v : zl, head zr, tail zr)
+zipperRight :: ZipperList a -> Maybe (ZipperList a)
+zipperRight (zl, v, zr) =
+    case zr of
+        [] -> Nothing -- right list is empty, can't go left any further
+        x:xs -> Just (v:zl, x, xs)
 
-zipperToList :: ([a], a, [a]) -> [a]
+zipperToList :: ZipperList a -> [a]
 zipperToList (zl, v, zr) = reverse zl ++ (v : zr)
 
+-- 2D list zipper functions
+zipper2Select :: ([ZipperList a], ZipperList a, [ZipperList a]) -> a
+zipper2Select (zl, v, zr) = zipperSelect v
+
+zipper2Set :: ZipperList (ZipperList a) -> a -> ZipperList (ZipperList a)
+zipper2Set (zl, v, zr) newVal = (zl, zipperSet v newVal, zr)
+
+zipper2Up :: ZipperList (ZipperList a) -> Maybe (ZipperList (ZipperList a))
+zipper2Up = zipperLeft
+
+zipper2Down :: ZipperList (ZipperList a) -> Maybe (ZipperList (ZipperList a))
+zipper2Down = zipperRight
+
+zipper2Left :: ZipperList (ZipperList a) -> Maybe (ZipperList (ZipperList a))
+zipper2Left (zl, v, zr) = do
+    newZl <- traverse zipperLeft zl
+    newV  <- zipperLeft v
+    newZr <- traverse zipperLeft zr
+    return (newZl, newV, newZr)
+
+zipper2Right :: ZipperList (ZipperList a) -> Maybe (ZipperList (ZipperList a))
+zipper2Right (zl, v, zr) = do
+    newZl <- traverse zipperRight zl
+    newV  <- zipperRight v
+    newZr <- traverse zipperRight zr
+    return (newZl, newV, newZr)
+
+zipper2ToList :: ZipperList (ZipperList a) -> [[a]]
+zipper2ToList z = map zipperToList (zipperToList z)
